0d86253b8d2f54e3e77bf46a4f189dbd0c2fd960
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-09-27 20:45:09 +0500

    handled disconnect

--- database/db.js
+++ database/db.js
@@ -1,13 +1,30 @@
 import mysql from 'mysql';
 
 export let db;
-try {
-        db = mysql.createConnection({
-        host: 'us-cdbr-east-06.cleardb.net',
-        user: 'bf12b8de10a36e',
-        password: '0e2e857c',
-        database: 'heroku_bc558aeaefc8cb6',
+
+const db_config = {
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'bf12b8de10a36e',
+    password: '0e2e857c',
+    database: 'heroku_bc558aeaefc8cb6',
+};
+
+function handleDisconnect() {
+    db = mysql.createConnection(db_config);
+
+    db.connect(function (err) {
+        if (err) {
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000);
+        }
+    })
+    
+    connection.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect()
+        }
     });
-} catch (error) {
-    console.log('cannot connect db');
 }
+
+handleDisconnect();
